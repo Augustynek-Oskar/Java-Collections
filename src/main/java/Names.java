3ad9fa85d8b3671e6f691edf2c458ce821cbc077
Oskar Augustynek <oskar.augustynek@gmail.com>
2023-12-02 14:53:02 +0100

    Method for showing list elements without duplicates.

--- src/main/java/Names.java
+++ src/main/java/Names.java
@@ -1,9 +1,9 @@
-import java.util.ArrayList;
-import java.util.List;
+import javax.xml.crypto.dsig.spec.XSLTTransformParameterSpec;
+import java.util.*;
 
 public class Names {
     public static void main(String[] args) {
-        List <String> names = new ArrayList<>();
+        List<String> names = new ArrayList<>();
         names.add("Anna");
         names.add("Katarzyna");
         names.add("Tymon");
@@ -13,22 +13,20 @@ public class Names {
         names.add("Dorota");
         names.add("Piotr");
 
-//        for (String name : names){
-//            System.out.println(name);
-//        }
-        showAllElementsOfArrayList(names);
+        showListElementsWithoutDuplicates(names);
+
     }
+
     public static void showAllElementsOfArrayList (List nameOfArrayList){
-        for (Object elements : nameOfArrayList){
+        for (Object elements : nameOfArrayList) {
             System.out.println(elements);
         }
     }
-    public static void showListElementsWithoutDuplicates (List nameOfArrayList){
-        for (Object elements : nameOfArrayList){
-            if (nameOfArrayList.contains(elements)  ){
-
-
-            }
-        }
+    public static void showListElementsWithoutDuplicates (List nameOfArrayList) {
+        Set <String> set = new LinkedHashSet<>();
+        set.addAll(nameOfArrayList);
+        nameOfArrayList.clear();
+        nameOfArrayList.addAll(set);
+        System.out.println(nameOfArrayList);
     }
 }
