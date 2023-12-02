799ba9b989dab3dd5f6d624b9a7c0c6d7f83ab1c
Oskar Augustynek <oskar.augustynek@gmail.com>
2023-12-02 02:29:28 +0100

    Array list with names

---
+++ src/main/java/Names.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Names {
+    public static void main(String[] args) {
+        List <String> names = new ArrayList<>();
+        names.add("Anna");
+        names.add("Katarzyna");
+        names.add("Tymon");
+        names.add("Tadeusz");
+        names.add("Anna");
+        names.add("Karolina");
+        names.add("Dorota");
+        names.add("Piotr");
+    }
+}
