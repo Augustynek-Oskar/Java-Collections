a6343d67f20ed49d618e619f203a2d3fa01c77c7
Oskar Augustynek <oskar.augustynek@gmail.com>
2023-12-05 16:30:44 +0100

    Interface for ownlist

---
+++ src/main/java/ArrayBasedList.java
@@ -0,0 +1,31 @@
+public class ArrayBasedList implements OwnList{
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public void add(Integer element) {
+
+    }
+
+    @Override
+    public Integer get(int i) {
+        return null;
+    }
+
+    @Override
+    public void add(int index, Integer element) {
+
+    }
+
+    @Override
+    public Integer remove(int index) {
+        return null;
+    }
+}
