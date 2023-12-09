a9a3446f7d0f606cabd9018f937d793646b2099c
Oskar Augustynek <oskar.augustynek@gmail.com>
2023-12-10 00:54:15 +0100

    Comparator class for weight and height

--- src/main/java/ListOfPeople.java
+++ src/main/java/ListOfPeople.java
@@ -2,15 +2,9 @@ import java.util.*;
 
 public class ListOfPeople {
     public static void main(String[] args) {
+        HeightComparator heightComparator = new HeightComparator();
+        WeightComparator weightComparator = new WeightComparator();
 
-//
-//
-//        LinkedList <Person> person = new LinkedList<>();
-//
-//        person.add(new Person("Adam", "Nowak", 1999, 185, 87));
-//        person.add(new Person("Jan", "Kowalski", 2002, 178, 75));
-//        person.add(new Person("Kacper", "Bętkowski", 1998, 173, 70));
-//        person.add(new Person("Michał", "Fąk", 2005, 190, 83));
 
         List <Person> people = new ArrayList<>();
 
@@ -19,13 +13,11 @@ public class ListOfPeople {
         people.add(new Person("Kacper", "Bętkowski", 1998, 173, 70));
         people.add(new Person("Michał", "Fąk", 2005, 190, 83));
 
-        System.out.println("unsorted: " + people);
-
-        HeightComparator heightComparator = new HeightComparator();
-
         Collections.sort(people, heightComparator);
+        System.out.println("Sorted from shortest to tallest: " + people);
 
-        System.out.println("sorted " + people);
+        Collections.sort(people, weightComparator);
+        System.out.println("Sorted from heaviest to lightest: " + people);
 
     }
 }
--- src/main/java/WeightComparator.java
+++ src/main/java/WeightComparator.java
@@ -1,8 +1,9 @@
+import java.util.Collections;
 import java.util.Comparator;
 
 public class WeightComparator implements Comparator<Person> {
     @Override
     public int compare(Person o1, Person o2) {
-        return Float.compare(o1.getWeight(), o2.getWeight());
+        return Float.compare(o2.getWeight(), o1.getWeight());
     }
 }
