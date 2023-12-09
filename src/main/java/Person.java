738bc59ad7f90371775a7c892d09804dbfc1c55e
Oskar Augustynek <oskar.augustynek@gmail.com>
2023-12-09 22:15:43 +0100

    Class Person

---
+++ src/main/java/Person.java
@@ -0,0 +1,15 @@
+public class Person {
+    String name;
+    String surname;
+    int yearOfBirth;
+    float height;
+    float weight;
+
+    public Person(String name, String surname, int yearOfBirth, float height, float weight) {
+        this.name = name;
+        this.surname = surname;
+        this.yearOfBirth = yearOfBirth;
+        this.height = height;
+        this.weight = weight;
+    }
+}
